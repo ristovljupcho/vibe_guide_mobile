f616875cd4426f501e280a6f874651f8eb7a824d
ristovljupcho <ristov.ljupcho@gmail.com>
2025-06-25 14:44:32 +0200

    Added data for PlacePreviewResponseDTO as JSON file.

---
+++ src/components/ui/place-card.tsx
@@ -0,0 +1,45 @@
+import React from "react";
+import Image from "next/image";
+import { StaticImageData } from "next/image";
+
+type PlaceCardProps = {
+  imageSrc: StaticImageData;
+  name: string;
+  rating: number;
+  primaryType: string;
+  topTraits: string[];
+};
+
+export default function PlaceCard({
+  imageSrc,
+  name,
+  rating,
+  primaryType,
+  topTraits,
+}: PlaceCardProps) {
+  return (
+    <div className="bg-[#202824] rounded-3xl w-[230px] shadow-md overflow-hidden text-white">
+      <Image
+        src={imageSrc}
+        alt="Logo"
+        width={150}
+        height={75}
+        className="block"
+      />
+      <div className="p-4">
+        <h2 className="text-xl font-serif">{name}</h2>
+        <p className="text-md text-gray-300">{rating}</p>
+        <p className="text-md text-gray-300">{primaryType}</p>
+        {topTraits.map((trait, index) => (
+          <a
+            key={index}
+            href="#"
+            className="text-xs text-white bg-gray-700 px-2 py-1 rounded-full hover:bg-gray-600"
+          >
+            {trait}
+          </a>
+        ))}
+      </div>
+    </div>
+  );
+}
