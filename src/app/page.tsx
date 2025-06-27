5c58fdb067b4285966c64953f97cbff5934a290d
ristovljupcho <ristov.ljupcho@gmail.com>
2025-06-27 14:57:49 +0200

    Added new data for daily offers and card for usage in the view.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,7 @@
 "use client";
 
-export default function Home() {}
+import DailyOffersView from "@/components/place-view/daily-offers-view";
+
+export default function Home() {
+  return <DailyOffersView />;
+}
